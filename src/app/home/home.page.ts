8fa8ed74f1f3791fee10a7d3f701be1cea1dae3e
PedroCandia <petercandiavazquez@hotmail.com>
2023-11-15 12:59:04 -0600

    animation to show extra information about the icons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,19 +8,24 @@ import { Component } from '@angular/core';
 export class HomePage {
   icons = [
     {
-      src: "../../assets/icon/logo_icon.png"
+      src: "../../assets/icon/logo_icon.png",
+      txt: '공식 블로그'
     },
     {
-      src: "../../assets/icon/tel_icon.png"
+      src: "../../assets/icon/tel_icon.png",
+      txt: '1668-4636'
     },
     {
-      src: "../../assets/icon/msg_icon.png"
+      src: "../../assets/icon/msg_icon.png",
+      txt: '채팅 상담'
     },
     {
-      src: "../../assets/icon/map_icon.png"
+      src: "../../assets/icon/map_icon.png",
+      txt: '오시는 길'
     },
     {
-      src: "../../assets/icon/doc_icon.png"
+      src: "../../assets/icon/doc_icon.png",
+      txt: '브로슈어'
     }
   ]
 
