451ed0ae4364f52b9e490d64e619e23e121b2a6e
PedroCandia <petercandiavazquez@hotmail.com>
2023-11-15 20:18:59 -0600

    icons data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,23 +9,30 @@ export class HomePage {
   icons = [
     {
       src: "../../assets/icon/logo_icon.png",
-      txt: '공식 블로그'
+      txt: '공식 블로그',
+      target: '_blank',
+      href: 'https://blog.naver.com/ktk0bx48'
     },
     {
       src: "../../assets/icon/tel_icon.png",
-      txt: '1668-4636'
+      txt: '1668-4636',
+      href: "tel:1668-4636"
     },
     {
       src: "../../assets/icon/msg_icon.png",
-      txt: '채팅 상담'
+      txt: '채팅 상담',
+      target: '_blank',
+      href: 'https://teheran.channel.io/home'
     },
     {
       src: "../../assets/icon/map_icon.png",
-      txt: '오시는 길'
+      txt: '오시는 길',
+      href: 'https://www.thr-law.co.kr/roadmap'
     },
     {
       src: "../../assets/icon/doc_icon.png",
-      txt: '브로슈어'
+      txt: '브로슈어',
+      href: 'https://www.thr-law.co.kr/images/bottom/teheran_brochure.pdf'
     }
   ]
 
