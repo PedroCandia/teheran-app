0f9b2bb91e23ad0ec8a8529c53b3dd8ee9719246
PedroCandia <petercandiavazquez@hotmail.com>
2023-11-20 00:03:39 -0600

    bugfix video section0

--- src/app/components/section0/section0.component.ts
+++ src/app/components/section0/section0.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-section0',
   templateUrl: './section0.component.html',
   styleUrls: ['./section0.component.scss'],
 })
-export class Section0Component  implements OnInit {
+export class Section0Component {
 
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
 
-  ngOnInit() {}
+  videoLoaded() {
+    const videoElement = this.elRef.nativeElement.querySelector('.video1') as HTMLVideoElement;
+    if (videoElement) {
+      videoElement.muted = true;
+      videoElement.play();
+    }
+  }
+  
+  videoError() {
+    console.error('Error al cargar el video.');
+  }  
 
 }
